6f863f7c11c303207b9423eed2232468f4e19507
Steven Lee <stevenwh.lee@gmail.com>
2025-06-26 22:49:27 +1000

    Convert it to a Jupyter notebook

--- attendance.py
+++ attendance.py
@@ -1,59 +1,52 @@
 #!/usr/bin/env python3
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.2
+# ---
 
+# %% [markdown]
 """
-Generates a graph using the attendance report from Elvanto
+Generates a graph using the attendance report from Elvanto.
 
 You first need to download the report manually. Export it as a CSV file from:
+```
 https://{elvanto_host}/admin/reports/report_existing/?category=services&report=service_reports_individuals&date_range_type=this_year&date_range_from=20%2F11%2F2024&date_range_to=26%2F11%2F2024&frequency=&attendance=attended&did_not_attend_times=&attended_times=&service_types_services=y&service_type%5B%5D=6f3a9664-435f-42fe-a841-28c53b7bcd97&service_type%5B%5D=aacac264-aedc-418b-a8c2-6aca498bbca6&include_individual_service=yes&multi_select_filter_demographic%5B%5D=&multi_select_filter_columns%5B%5D=demo&columns%5B%5D=category_id&columns%5B%5D=demographic_id&sort=0&order=asc&output=web&export_orientation=l&export_size=&export_labels_address=
+```
 
 Usage:
-    ./attendance.py -f path/to/Service-Individual-Attendance-1-Jan-2025-31-Dec-2025.csv
+```
+./attendance.py -f path/to/Service-Individual-Attendance-1-Jan-2025-31-Dec-2025.csv
+```
 
+To keep the Jupyter Notebook in sync:
+```
+jupytext --set-formats ipynb,py attendance.py --sync
+```
 """
 
+# %%
 import argparse
 import datetime as dt
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import re
+import sys
 
 
-def main():
-    args = parse_args()
-
-    print(f"Processing file: {args.file}")
-    csv_data = pd.read_csv(args.file)
-    individual_attendance = preprocess_individual_attendance_data(csv_data)
-
-    # Create attendance columns
-    individual_attendance["Total"] = True
-
-    individual_attendance.loc[
-        individual_attendance["Demographics"].eq("Adults"), "Adults"
-    ] = True
-
-    individual_attendance.loc[
-        individual_attendance["People Category"].isin(
-            ["Newcomers", "Visitors / New People"]
-        ),
-        "Newcomers/Visitors",
-    ] = True
-
-    # We don't need this data anymore
-    individual_attendance.drop(
-        columns=["People Category", "Demographics"],
-        inplace=True,
-    )
-
-    # Each of the attendance columns will get their own count
-    attendance_summary = individual_attendance.groupby("Date").count()
-
-    print(attendance_summary)
-    generate_graph(attendance_summary)
+# %% [markdown]
+# Parse args
+# ----------
 
 
-def parse_args():
+# %%
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Generate a graph from the Elvanto attendance report"
     )
@@ -64,12 +57,26 @@ def parse_args():
 
     if not os.path.isfile(args.file):
         raise SystemExit(f"error: No such file exists: {args.file}")
-
     return args
 
 
-def preprocess_individual_attendance_data(csv_data: pd.DataFrame) -> pd.DataFrame:
-    def to_date_str(column_name: str) -> str:
+# sys.argv = ["attendance.py", "-f", "data/Service-Individual-Attendance-2025-06-24.csv"]
+args = parse_args()
+args
+
+
+# %% [markdown]
+# Clean and normalize the attendance report
+# -----------------------------------------
+# * Unpivot the table so that each column representing the attendance for each
+#   week is converted to a single column for attendance and another column to
+#   indicate the service date.
+# * Extract the date out of the service names and convert them to date objects.
+
+
+# %%
+def clean_and_normalize_attendance_report(csv_data: pd.DataFrame) -> pd.DataFrame:
+    def service_name_to_date_str(column_name: str) -> str:
         dd_mm = re.search(r"(\d\d/\d\d)", column_name)
         return (
             f"{ dd_mm.group() }/{ dt.date.today().year }"
@@ -77,9 +84,7 @@ def preprocess_individual_attendance_data(csv_data: pd.DataFrame) -> pd.DataFram
             else column_name  # All other column names remain the same
         )
 
-    data = csv_data.drop(
-        columns=["First Name", "Last Name", "Attended", "Absent"]
-    ).rename(columns=to_date_str)
+    data = csv_data.rename(columns=service_name_to_date_str)
 
     # Unpivot the date-attendance columns into rows.
     # The date column names become the values in a new Date column, and
@@ -105,11 +110,56 @@ def preprocess_individual_attendance_data(csv_data: pd.DataFrame) -> pd.DataFram
     return individual_attendance
 
 
-def generate_graph(attendance_summary: pd.DataFrame):
-    attendance_summary.plot()
-    plt.grid()
-    plt.show()
+print(f"Processing file: {args.file}")
+csv_data = pd.read_csv(args.file).drop(
+    columns=["First Name", "Last Name", "Attended", "Absent"]
+)
+individual_attendance = clean_and_normalize_attendance_report(csv_data)
+individual_attendance
 
 
-if __name__ == "__main__":
-    main()
+# %% [markdown]
+# Create the attendance columns
+# -----------------------------
+
+# %%
+individual_attendance["Total"] = True
+
+individual_attendance.loc[
+    individual_attendance["Demographics"] == "Adults", "Adults"
+] = True
+
+individual_attendance.loc[
+    individual_attendance["People Category"].isin(
+        ["Newcomers", "Visitors / New People"]
+    ),
+    "Newcomers/Visitors",
+] = True
+
+# We don't need this data anymore
+individual_attendance.drop(
+    columns=["People Category", "Demographics"],
+    inplace=True,
+)
+
+individual_attendance
+
+
+# %% [markdown]
+# Summarise (aggregate) the data
+# ------------------------------
+
+# %%
+# Each of the attendance columns will get their own count
+attendance_summary = individual_attendance.groupby("Date").count()
+attendance_summary
+
+
+# %% [markdown]
+# Generate the graph
+# ------------------
+
+# %%
+attendance_summary.plot()
+plt.grid()
+plt.show()
